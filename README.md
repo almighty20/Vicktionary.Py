430674535c41f25eecd50dd2c02e90b59ab44aaf
Almighty <iamvikal901@gmail.com>
2023-02-08 18:58:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wicktionary
+
+
+#### Introduction:
+
+Vicktionary: a simple and sophisticated next-gen dictionary.
+
+#### Features:
+
++ Lightning Fast Search
++ Rich UI
++ Simple Design
++ Integrated Image Results for Smart Searches
++ Of course, Zero Advertisements
+
+## App in Action:
---
+++ main.py
@@ -0,0 +1,25 @@
+import flet as ft
+from routing import AppRouting
+
+def main(page: ft.Page):
+    # setting the app title
+    page.title = "Vicktionary"
+
+    # enabling scroll in the page
+    page.auto_scroll = True
+
+    # setting the app background color
+    page.theme_mode = "light"
+    page.theme = ft.theme.Theme(color_scheme_seed="#038F75")
+
+    # setting the custom font
+    page.fonts = {
+        "CabinSketchBold": "fonts/CabinSketchBold.ttf",
+        "CabinSketchRegular": "fonts/CabinSketchRegular.ttf"
+    }
+
+    app = AppRouting(page)
+
+    page.add(app)
+
+ft.app(target=main, view=ft.WEB_BROWSER, assets_dir="assets")
